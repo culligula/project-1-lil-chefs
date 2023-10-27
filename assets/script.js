1b50cf3dcf37ed7f1deb1106b9f91132055a6d85
TylerFruik <tylerfruik@gmail.com>
2023-10-26 21:15:39 -0500

    JS | Added functionality to the navigation buttons between menus.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
+const mainMenuBtn = document.getElementById('return-to-main-menu');
+const ingredientsMenuBtn = document.getElementById('ingredients-menu-btn');
+const newRecipesBtn = document.getElementById('find-recipes-btn');
+const viewSavedRecipesBtn = document.getElementById('view-saved-recipes-btn');
+const mainMenuBtn2 = document.getElementById('return-to-main-menu-2');
+
+const mainMenu = document.getElementById('main-menu');
+const ingredientsMenu = document.getElementById('ingredients-menu');
+const newRecipesMenu = document.getElementById('new-recipes-menu');
+const savedRecipesMenu =document.getElementById('saved-recipes-menu');
+
 // var gets = function (user) {
 //     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?'
 
 //     fetch(apiUrl)
 //         .then(function (response) {
-
-
 
 function geoLocation() {
     const status = document.querySelector("#status");
@@ -35,4 +44,53 @@ function geoLocation() {
     }
 }
 
-document.querySelector("#find").addEventListener("click", geoLocation);
+// #region MENU NAVIGATION
+function showMainMenu() {
+  mainMenu.style.display = 'block';
+  ingredientsMenu.style.display = 'none';
+  newRecipesMenu.style.display = 'none';
+  savedRecipesMenu.style.display = 'none';
+}
+
+function showIngredientsMenu() {
+  mainMenu.style.display = 'none';
+  ingredientsMenu.style.display = 'block';
+  newRecipesMenu.style.display = 'none';
+  savedRecipesMenu.style.display = 'none';
+}
+
+function showNewRecipesMenu() {
+  mainMenu.style.display = 'none';
+  ingredientsMenu.style.display = 'none';
+  newRecipesMenu.style.display = 'block';
+  savedRecipesMenu.style.display = 'none';
+}
+
+function showSavedRecipesMenu() {
+  mainMenu.style.display = 'none';
+  ingredientsMenu.style.display = 'none';
+  newRecipesMenu.style.display = 'none';
+  savedRecipesMenu.style.display = 'block';
+}
+
+mainMenuBtn.addEventListener('click', () => {
+  showMainMenu();
+})
+
+ingredientsMenuBtn.addEventListener('click', () => {
+  showIngredientsMenu();
+})
+
+
+newRecipesBtn.addEventListener('click', () => {
+  showNewRecipesMenu();
+})
+
+viewSavedRecipesBtn.addEventListener('click', () => {
+  showSavedRecipesMenu();
+})
+
+mainMenuBtn2.addEventListener('click', () => {
+  showMainMenu();
+})
+//#endregion
