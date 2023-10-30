d9923790404a55c93fe2281a609b5deb7367f062
Lady Godiva <watson.amber.denise@gmail.com>
2023-10-30 16:03:45 -0700

    updated with api for lat and long with button

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,8 @@
-// var gets = function (user) {
-//     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?'
-
-//     fetch(apiUrl)
-//         .then(function (response) {
 
 
+var location = document.querySelector("#find-location-btn");
 
+//template from MDN: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
 function geoLocation() {
     const status = document.querySelector("#status");
     const map = document.querySelector("#map");
@@ -37,3 +34,21 @@ function geoLocation() {
 
 document.querySelector("#find").addEventListener("click", geoLocation);
 
+var gets = function (user) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=343936b9fd05267869e0bf8c1d533d1c';
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayRecipes(data);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to recommend recipes');
+        });
+};
