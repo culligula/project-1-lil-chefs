39688aba27ea3b45bfe5cb800045a076149eec54
culligula <colt_culpepper@yahoo.com>
2023-10-30 22:03:57 -0400

    food api completed

--- assets/js/food-api.js
+++ assets/js/food-api.js
@@ -25,14 +25,13 @@ concatenateButtons.forEach(button => {
   });
 });
 
-
-const query = concatenatedValue
-const apiUrl = `https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&q=${query}`;
-
-
 document.getElementById("find-recipes-btn").addEventListener("click", function () {
+  const query = concatenatedValue;
+  const apiKey = "da196eedb5msh00e79c58139ed2ap1d41a0jsn4c2725d1a2ec";
+  const apiUrl = `https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&q=${query}`;
+
   var myHeaders = new Headers();
-  myHeaders.append("X-RapidAPI-Key", "da196eedb5msh00e79c58139ed2ap1d41a0jsn4c2725d1a2ec");
+  myHeaders.append("X-RapidAPI-Key", apiKey);
   myHeaders.append("X-RapidAPI-Host", "edamam-recipe-search.p.rapidapi.com");
 
   var requestOptions = {
@@ -41,20 +40,35 @@ document.getElementById("find-recipes-btn").addEventListener("click", function (
     redirect: 'follow'
   };
 
-  fetch(apiUrl,requestOptions)
-  .then(response => {
-    if (response.ok) {
-      return response.json(); // Assuming the API returns JSON data
-    } else {
-      throw new Error('API request failed');
-    }
-  })
-  .then(data => {
-    // Process the API response data
-    const searchResultsEl = document.getElementById('new-recipes-menu');
-    searchResultsEl.textContent = JSON.stringify(data, null, 2);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-  })
+  fetch(apiUrl, requestOptions)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('API request failed');
+      }
+    })
+    .then(data => {
+      // Process the API response data
+      const searchResultsEl = document.getElementById('new-recipes-menu');
+      console.log(data);
+
+      // Loop through the recipe hits and display them
+      data.hits.forEach(hit => {
+        const recipe = hit.recipe;
+        const recipeName = recipe.label;
+        const recipeLink = recipe.url;
+
+        // Create an HTML element to display each recipe
+        const recipeElement = document.createElement("div");
+        recipeElement.innerHTML = `<a href="${recipeLink}" target="_blank">${recipeName}</a>`;
+        
+        // Append the recipe element to the results container
+        searchResultsEl.appendChild(recipeElement);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
+
