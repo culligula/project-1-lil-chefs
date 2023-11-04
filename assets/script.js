d6d10f9610b8eb49a765801e77879adceff5cb19
TylerFruik <tylerfruik@gmail.com>
2023-11-03 21:09:39 -0500

    Merged the js files and furthered food search result sorting

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,101 @@ let recipesList = [];
 let longitude = 0;
 let latitude = 0;
 
+// #region FOOD API
+// get the ingredient
+const searchButtons = document.querySelectorAll(".btn");
+const exclusionButtons = document.querySelectorAll(".btn-2");
+// print the selected ingredients to an empty container, then use that string
+// as a search query in the call to the api
+
+let searchValue = "";
+const emptySpace = String.fromCharCode(32);
+let exclusionValue = "";
+searchButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    searchValue += button.value;
+    button.classList.add("selected");
+    console.log("selected button", button.value);
+  });
+});
+exclusionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    exclusionValue += button.value;
+  });
+});
+// api call, initiated on click
+function recipeListGen() {
+  document
+    const query = searchValue;
+    const exclusions = exclusionValue;
+    const apiKey = "da196eedb5msh00e79c58139ed2ap1d41a0jsn4c2725d1a2ec";
+    const apiUrl = `https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&q=${query}&excluded=${exclusions}`;
+    var myHeaders = new Headers();
+    myHeaders.append("X-RapidAPI-Key", apiKey);
+    myHeaders.append("X-RapidAPI-Host", "edamam-recipe-search.p.rapidapi.com");
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    fetch(apiUrl, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("API request failed");
+        }
+      })
+      .then((data) => {
+        const searchResultsEl = document.getElementById("new-recipes-menu");
+        console.log(data);
+        data.hits.forEach((hit) => {
+          const recipe = hit.recipe;
+          const recipeName = recipe.label;
+          const recipeLink = recipe.url;
+          const recipeElement = document.createElement("div");
+          recipeElement.innerHTML = `
+          <a href="${recipeLink}" target="_blank">${recipeName}</a>
+          <button class="save-button" data-recipe='${JSON.stringify(
+            recipe
+          )}'>Save</button>
+        `;
+          // Add a click event listener to the "Save" button
+          const saveButton = recipeElement.querySelector(".save-button");
+          saveButton.addEventListener("click", () => {
+            const savedRecipes = JSON.parse(
+              localStorage.getItem("savedRecipes") || "[]"
+            );
+            savedRecipes.push(recipe);
+            localStorage.setItem("savedRecipes", JSON.stringify(savedRecipes));
+            console.log("Recipe saved to local storage.");
+          });
+          searchResultsEl.appendChild(recipeElement);
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+  });
+}
+// TODO: add save button next to result, and save that result to local storage
+const savedRecipes = JSON.parse(localStorage.getItem("savedRecipes") || "[]");
+if (savedRecipes.length > 0) {
+  const container = document.getElementById("saved-recipes-list");
+  container.innerHTML = "";
+  savedRecipes.forEach((recipe) => {
+    const recipeEl = document.createElement("div");
+    recipeEl.innerHTML = `
+      <a href="${recipe.url}" target="_blank">${recipe.label}</a>
+    `;
+    container.appendChild(recipeEl);
+  });
+} else {
+  const container = document.getElementById("saved-recipes-list");
+  container.innerHTML = "<p>No saved recipes yet.</p>";
+}
+// #endregion
+
+// #region LOCATION AND WEATHER API
 //template for locating user coordinates from MDN: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
 function geoLocation() {
   let status = document.querySelector("#status");
@@ -71,6 +166,7 @@ function geoLocation() {
       });
   };
 }
+// #endregion
 
 // #region MENU NAVIGATION
 function resetConditions() {
@@ -138,9 +234,11 @@ yesBtn.addEventListener("click", () => {
   showFindRecipesMenu();
 });
 
-noBtn.addEventListener("click", showFindRecipesMenu);
+noBtn.addEventListener("click", () => {
+  showFindRecipesMenu()
+  recipeListGen()
+});
 //#endregion
-
 
 // #region RECIPE SORTING BASED ON WEATHER
 function setRecipeSelectionFromWeather() {
@@ -149,33 +247,32 @@ function setRecipeSelectionFromWeather() {
   if (weatherMain == 'Rain' || weatherMain == 'Drizzle' || weatherMain == 'Thunderstorm' || weatherMain == 'Snow') {
     pointer = 'soup'
     //dish.type
-    generateRecipesFromWeather(pointer);
+    recipeListGen();
   } else if (weatherMain == 'Clear' || weatherMain == 'Cloudy') {
       if (weatherTemp >= 70) {
         pointer = 'sandwiches';
         console.log(pointer);
         // dish.type
-        generateRecipesFromWeather(pointer);
+        recipeListGen();
       } else if (weatherTemp < 70 & weatherTemp >= 40) {
         pointer = 'main-course';
         console.log(pointer);
         // meal.type
-        generateRecipesFromWeather(pointer);
+        recipeListGen();
       } else if (weatherTemp < 40 & weatherTemp >= 10) {
         pointer = 'soup';
         console.log(pointer);
         //dish.type
-        generateRecipesFromWeather(pointer);
+        recipeListGen();
       } else {
         pointer = 'pancake';
         console.log(pointer);
         //dish.type
+        recipeListGen();
     }
   } else {
     //Emergency serevices
+    recipeListGen();
   }
 }
-
-function generateRecipesFromWeather(pointer) {
-
-}
+// #endregion
